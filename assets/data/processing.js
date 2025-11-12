44b09064beb931262c400f69c602b8bea85853f3
FragosoQ <qualidade@somengil.com>
2025-11-12 15:51:54 +0000

    Update processing.js

--- assets/data/processing.js
+++ assets/data/processing.js
@@ -100,15 +100,24 @@ function selectCountries(list, countries) {
 
 // Connections
 
-const connections = {
+/*const connections = {
   'Colombia': ['Ecuador', 'Cuba', 'Mexico', 'Peru', 'Venezuela, RB', 'Guyana', 'United States'],
   'South Sudan': ['Nigeria', 'Sudan', 'Kenya', 'Uganda', 'Zambia', 'Malawi', 'Ethiopia', 'Somalia', 'Madagascar', 'Yemen, Rep.'],
   'India': ['Pakistan', 'Kazakhstan', 'Maldives', 'Sri Lanka', 'Vietnam', 'Thailand'],
   'Thailand': ['Singapore', 'Indonesia', 'Nepal', 'Vietnam', 'Sri Lanka', 'Cambodia', 'Pakistan'],
   'Panama': ['Cuba', 'Mexico', 'Ecuador', 'Colombia', 'Peru', 'Venezuela, RB', 'United States'],
   'Fiji': ['Tuvalu', 'Nauru', 'Kiribati', 'Tonga', 'New Caledonia', 'New Zealand']
-}
+}*/
 
+const connections = {
+  "Portugal": ["Colombia", "South Sudan", "India", "Thailand", "Panama", "Fiji"],
+  "Colombia": ["Ecuador", "Cuba", "Mexico", "Peru", "Venezuela, RB", "Guyana", "United States"],
+  "South Sudan": ["Nigeria", "Sudan", "Kenya", "Uganda", "Zambia", "Malawi", "Ethiopia", "Somalia", "Madagascar", "Yemen, Rep."],
+  "India": ["Pakistan", "Kazakhstan", "Maldives", "Sri Lanka", "Vietnam", "Thailand"],
+  "Thailand": ["Singapore", "Indonesia", "Nepal", "Vietnam", "Sri Lanka", "Cambodia", "Pakistan"],
+  "Panama": ["Cuba", "Mexico", "Ecuador", "Colombia", "Peru", "Venezuela, RB", "United States"],
+  "Fiji": ["Tuvalu", "Nauru", "Kiribati", "Tonga", "New Caledonia", "New Zealand"]
+};
 
 function getCountry(name, countries) {
   return countries.find(c => c.name === name);
